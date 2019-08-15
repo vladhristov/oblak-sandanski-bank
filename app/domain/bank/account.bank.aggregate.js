c514c091bc9742cb756667e9c69c405bb4a00c67
Vladislav Hristov <vlad@Admins-MacBook-Pro.local>
2019-08-15 12:19:53 +0300

    feat: bank aggregate

--- app/domain/bank/account.bank.aggregate.js
+++ app/domain/bank/account.bank.aggregate.js
@@ -1,14 +1,48 @@
 'use strict';
 
-const commands = {
+const { domain: { command: { only } } } = require('oblak/lib/tools');
 
+const initialState = {
+	balance: 0, // current balance in stotinki
+	pin: '', // pin code of the account
+	holder: '', // account beneficiary
+};
+
+const commands = {
+	create: [
+		async (cmd, agg, app) => {
+			const { payload, metadata } = cmd;
+			const { holder, pin } = payload;
+			agg.apply.created({ holder, pin });
+		},
+	],
+	deposit: [
+		() => {},
+	],
+	withdraw: [
+		() => {},
+	],
 };
 
 const events = {
-
+	created: [
+		(evt, aggregate) => {
+			const { payload } = evt;
+			const { pin, holder } = payload;
+			aggregate.set('pin', pin);
+			aggregate.set('holder', holder);
+		},
+	],
+	deposited: [
+		() => {},
+	],
+	withdrawn: [
+		() => {},
+	],
 };
 
 module.exports = {
+	initialState,
 	commands,
-	exports,
+	events,
 };
