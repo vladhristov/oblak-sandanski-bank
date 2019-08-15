3234979c9f1c3b86071ae95f362b464d2f42537c
Vladislav Hristov <vlad@Admins-MacBook-Pro.local>
2019-08-15 17:23:29 +0300

    feat: get routes

--- app/gateways/rest/bank/routes/accounts.bank.route.js
+++ app/gateways/rest/bank/routes/accounts.bank.route.js
@@ -1,15 +1,39 @@
 'use strict';
 
-const { async, command } = require('oblak').tools.rest;
+const { async, command, readmodels } = require('oblak').tools.rest;
 
 const routes = {
 	'/': {
+		get: readmodels.list(r => r.bank.account),
 		post: [
 			async(async (req, res) => {
 				const app = req.getApp();
 				const { pin, holder } = req.body;
 				const { payload } = await app.getDomain().bank.account().create({ pin, holder }).await('readmodel.bank.account.*');
 				res.json(payload);
+			}),
+		],
+	},
+	'/:account': {
+		get: [
+			async(async (req, res) => {
+				const app = req.getApp();
+				const { account } = req.params;
+				const [accountResult] = await app.getReadmodels().bank.account(account);
+				if (!accountResult)
+					throw new app.errors.NotFoundError();
+				res.json(accountResult);
+			}),
+		],
+	},
+	'/:account/transactions': {
+		get: [
+			async(async (req, res) => {
+				const app = req.getApp();
+				const { account } = req.params;
+				res.json(await app.getReadmodels().bank.transaction()
+					.filter({ account })
+					.sort([{ timestamp: 'asc' }]));
 			}),
 		],
 	},
